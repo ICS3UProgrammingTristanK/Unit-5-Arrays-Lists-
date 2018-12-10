99b8811012ec46f896340e6e5d4bf9961706df6f
Tristan Kalabric <32547188+TristanKalabric@users.noreply.github.com>
2018-12-10 10:53:02 -0500

    created 2D Array program

---
+++ 2DArrayTristanK/2DArrayTristanK/2DArrayForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace _2DArrayTristanK
+{
+    partial class frm2DArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.nudLength = new System.Windows.Forms.NumericUpDown();
+            this.nudWidth = new System.Windows.Forms.NumericUpDown();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtArray = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLength.Location = new System.Drawing.Point(12, 22);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(48, 16);
+            this.lblLength.TabIndex = 0;
+            this.lblLength.Text = "Length";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWidth.Location = new System.Drawing.Point(12, 62);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(42, 16);
+            this.lblWidth.TabIndex = 1;
+            this.lblWidth.Text = "Width";
+            // 
+            // nudLength
+            // 
+            this.nudLength.Location = new System.Drawing.Point(66, 22);
+            this.nudLength.Name = "nudLength";
+            this.nudLength.Size = new System.Drawing.Size(89, 20);
+            this.nudLength.TabIndex = 2;
+            // 
+            // nudWidth
+            // 
+            this.nudWidth.Location = new System.Drawing.Point(66, 62);
+            this.nudWidth.Name = "nudWidth";
+            this.nudWidth.Size = new System.Drawing.Size(89, 20);
+            this.nudWidth.TabIndex = 3;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(172, 41);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(72, 23);
+            this.btnCreate.TabIndex = 4;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(54, 98);
+            this.txtArray.Multiline = true;
+            this.txtArray.Name = "txtArray";
+            this.txtArray.ReadOnly = true;
+            this.txtArray.Size = new System.Drawing.Size(147, 151);
+            this.txtArray.TabIndex = 5;
+            // 
+            // frm2DArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 261);
+            this.Controls.Add(this.txtArray);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.nudWidth);
+            this.Controls.Add(this.nudLength);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblLength);
+            this.Name = "frm2DArray";
+            this.Text = "2D Array";
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.NumericUpDown nudLength;
+        private System.Windows.Forms.NumericUpDown nudWidth;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtArray;
+    }
+}
+
---
+++ 2DArrayTristanK/2DArrayTristanK/2DArrayForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2DArrayTristanK
+{
+    public partial class frm2DArray : Form
+    {
+        public frm2DArray()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            //Declare local variables
+            int width;
+            int length;
+            Random randomNumberGenerator = new Random();
+            int aRandomNumber;
+            string aPieceOfText = null;
+            int widthCounter = 0;
+            int lengthCounter;
+
+            //Get the desired width and length from the user
+            width = Convert.ToInt16(this.nudWidth.Value);
+            length = Convert.ToInt16(this.nudLength.Value);
+
+            //Declare the array with the desired width and length
+            int[,] a2DArray = new int[width, length];
+
+            //Loop through each element in the width
+            for(lengthCounter = 0; widthCounter < width; widthCounter++)
+            {
+                //Loop through each element in the length
+                for(lengthCounter = 0; lengthCounter < length; lengthCounter++)
+                {
+                    //Get a random number between 0 and 9
+                    aRandomNumber = randomNumberGenerator.Next(0, 9 + 1);
+
+                    //Insert the random number into the array and the current width and length
+                    a2DArray[widthCounter, lengthCounter] = aRandomNumber;
+
+                    //Add the random number to the string of array numbers
+                    aPieceOfText = aPieceOfText + " " + aRandomNumber;
+                }
+                //Add a line breack to the end of the line to show a new row in the string
+                aPieceOfText = aPieceOfText + "\r" + "\n";
+            }
+            //Insert the string into the textbox
+            this.txtArray.Text = aPieceOfText;
+        }
+    }
+}
