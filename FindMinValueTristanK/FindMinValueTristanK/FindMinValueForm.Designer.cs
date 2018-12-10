fcafa471b1caf01789cb4b1db5e4431641590e7c
Tristan Kalabric <32547188+TristanKalabric@users.noreply.github.com>
2018-12-10 10:21:06 -0500

    created minValue program

---
+++ FindMinValueTristanK/FindMinValueTristanK/FindMinValueForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FindMinValueTristanK
+{
+    partial class frmFindMinValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstValues = new System.Windows.Forms.ListBox();
+            this.lblMinValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(94, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstValues
+            // 
+            this.lstValues.FormattingEnabled = true;
+            this.lstValues.Location = new System.Drawing.Point(56, 41);
+            this.lstValues.Name = "lstValues";
+            this.lstValues.Size = new System.Drawing.Size(157, 134);
+            this.lstValues.TabIndex = 1;
+            // 
+            // lblMinValue
+            // 
+            this.lblMinValue.AutoSize = true;
+            this.lblMinValue.Location = new System.Drawing.Point(53, 217);
+            this.lblMinValue.Name = "lblMinValue";
+            this.lblMinValue.Size = new System.Drawing.Size(57, 13);
+            this.lblMinValue.TabIndex = 2;
+            this.lblMinValue.Text = "Min Value:";
+            // 
+            // frmFindMinValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblMinValue);
+            this.Controls.Add(this.lstValues);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmFindMinValue";
+            this.Text = "Find Min Value";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstValues;
+        private System.Windows.Forms.Label lblMinValue;
+    }
+}
+
---
+++ FindMinValueTristanK/FindMinValueTristanK/FindMinValueForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMinValueTristanK
+{
+    public partial class frmFindMinValue : Form
+    {
+
+        //Declare global random number generator
+        Random randomNumberGenerator;
+
+        public frmFindMinValue()
+        {
+            InitializeComponent();
+            //Create random object
+            randomNumberGenerator = new Random();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Declare local variables and constants
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_RANDOM_NUMBER = 500;
+            int[] arrayOfIntegers = new int[MAX_ARRAY_SIZE];
+            int counter, randomNumber, minValue;
+
+            //Clear the listbox
+            this.lstValues.Items.Clear();
+
+            //Populating the array and listbox
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                //Generate a new random number between 1 and MAX_RANDOM_NUMBER
+                randomNumber = randomNumberGenerator.Next(1, MAX_RANDOM_NUMBER + 1);
+
+                //Assign the random number to the cell at position "counter" in the array
+                arrayOfIntegers[counter] = randomNumber;
+
+                //Add the random number to the listbox
+                this.lstValues.Items.Add(randomNumber);
+
+                //Refresh the form to display the new item in the listbox
+                this.Refresh();
+            }
+
+            //Set the min value
+            minValue = GetMinValue(arrayOfIntegers);
+
+            //Display minValue in a label
+            this.lblMinValue.Text = "The min value is: " + minValue;
+        }
+
+        private int GetMinValue (int[] tmpArrayOfIntegers)
+        {
+            int counter, tmpMinValue = 501;
+
+            //Finding the smallest number in the array
+            for(counter = 0; counter < tmpArrayOfIntegers.Length; counter++)
+            {
+                if(tmpArrayOfIntegers[counter] < tmpMinValue)
+                {
+                    tmpMinValue = tmpArrayOfIntegers[counter];
+                }
+            }
+            return tmpMinValue;
+        }
+    }
+}
